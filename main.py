020b240fd7aab9ac6324b53fac4f72dd23fc67b6
andreaej <andreaej@users.noreply.github.com>
2019-10-09 11:13:18 -0400

    Add skeleton for Zoo class

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 #Your algorithm should go here OR you should use comments throughout
 
 #Your code goes here
+class Zoo:
+  
+  #__init___ method initializes the attributes of the instance variables
+  def __init__(...):
+    
+    
+def main():
+  #Fill in the details
+  
+  #Create an instance of the Zoo class
+  zoo_animal = Zoo(...)
+  
+  
+main()
