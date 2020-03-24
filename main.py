2446cbd212d936c82010f32fc82a3f816ba84365
tricheli <60237371+tricheli@users.noreply.github.com>
2020-03-24 14:47:45 -0400

    Update main.py
    submitted this in the wrong branch last time sorry I have been having issues with Github

--- main.py
+++ main.py
@@ -1,19 +1,75 @@
-#Your name goes here
+#Tenisce Richelieu 
 
 #Your algorithm should go here OR you should use comments throughout
 
-#Your code goes here
+#In this class, you will create all the methods and attributes necessary to store and get
+#the name, animal type, age, sound and color of the animals.
+
 class Zoo:
-  
-  #__init___ method initializes the attributes of the instance variables
-  def __init__(...):
-    
+    #__init___ method initializes the attributes of the instance variables
+    def __init__(self):
+        self.name = ''
+        self.animalType = ''
+        self.age = 0
+        self.sound = ''
+        self.color = ''
+        
+    def storeName(self, inName):
+        self.name = inName
+
+    def storeAnimalType(self, inAnimalType):
+        self.animalType = inAnimalType
+
+    def storeAge(self, inAge):
+        self.age = inAge
+
+    def storeSound(self, inSound):
+        self.sound = inSound
+
+    def storeColor(self, inColor):
+        self.color = inColor
+
+    def printName(self):
+        print('My name is', self.name)
+
+    def printAnimalType(self):
+        print('I am a', self.animalType)
+        
+    def printAge(self):
+        print('My age is', self.age)
+
+    def printSound(self):
+        print('My sound is', self.sound)
+
+    def printColor(self):
+        print('I am', self.color)
+
     
 def main():
-  #Fill in the details
-  
-  #Create an instance of the Zoo class
-  zoo_animal = Zoo(...)
-  
-  
-main()
+    #Create an instance of the Zoo class
+    zoo_animal = Zoo()
+    
+    name = input('What is the name of the animal?: ')
+    zoo_animal.storeName(name)
+    animalType = input('What type of animal is it?: ')
+    zoo_animal.storeAnimalType(animalType)
+    age = int(input('How old is the animal?: '))
+    zoo_animal.storeAge(age)
+    sound = input('What sound does this animal make?: ')
+    zoo_animal.storeSound(sound)
+    color = input('What color(s) is this animal?: ')
+    zoo_animal.storeColor(color)
+    '''
+    storeName(self, inName)
+    storeAnimalType(self, inAnimalType)
+    storeAge(self, inAge)
+    storeSound(self, inSound)
+    storeColor(self, inColor)
+    '''
+    zoo_animal.printName()
+    zoo_animal.printAnimalType()
+    zoo_animal.printAge()
+    zoo_animal.printSound()
+    zoo_animal.printColor()
+    
+
